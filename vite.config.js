1597101a08ee0f42074896a60897b98b3a0bb69f
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2024-12-21 14:42:16 +0700

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Thư mục output mà Vite sẽ build vào
+    outDir: 'dist',
   },
 })
