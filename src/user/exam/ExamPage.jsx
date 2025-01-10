c22bf03174dd6d81662f545aaf23f7cf285aed4b
quanhp <hpq833592@gmail.com>
2025-01-10 23:53:23 +0700

    done nha

--- src/user/exam/ExamPage.jsx
+++ src/user/exam/ExamPage.jsx
@@ -1,8 +1,9 @@
-import { Modal, Button } from "antd";
+import { Button, Modal, Spin } from "antd";
 import React, { useState } from "react";
 import { BsFillStopFill } from "react-icons/bs";
 import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { Cell, Pie, PieChart } from "recharts";
 import "./ExamPage.scss";
 import ExamCard from "./components/ExamCard.jsx";
 import QuestionCard from "./components/QuestionCard";
@@ -22,7 +23,6 @@ const ExamPage = () => {
   const [examTitle, setExamTitle] = useState("Exam Title Not Available");
   const [resultData, setResultData] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
-
   const totalQuestions = questions.length;
 
   const sections = [
@@ -46,8 +46,8 @@ const ExamPage = () => {
     const section = getCurrentSection();
     if (!section) return [];
     const sectionStartIndex = sections
-        .slice(0, section.sectionIndex)
-        .reduce((sum, s) => sum + s.count, 0);
+      .slice(0, section.sectionIndex)
+      .reduce((sum, s) => sum + s.count, 0);
     const sectionEndIndex = sectionStartIndex + section.count;
     return questions.slice(sectionStartIndex, sectionEndIndex);
   };
@@ -101,6 +101,7 @@ const ExamPage = () => {
   };
 
   const handleFinishExam = async () => {
+    setLoading(true);
     try {
       const answerData = Object.entries(selectedAnswers).map(([index, answerValue]) => ({
         userId: Number(userId),
@@ -117,6 +118,7 @@ const ExamPage = () => {
           title: "Submission Error",
           content: "An error occurred while submitting your test. Please try again.",
         });
+        setLoading(false);
         return;
       }
 
@@ -132,7 +134,7 @@ const ExamPage = () => {
           title: "Test Submission Error",
           content: "There was an error with the test results. Please try again.",
         });
-        return;
+        setLoading(false);
       }
 
       setResultData(testResponse.data);
@@ -144,7 +146,9 @@ const ExamPage = () => {
         content: "Please check again and submit.",
       });
     }
+    setLoading(false);
   };
+
 
   const handleModalOk = () => {
     setIsModalVisible(false);
@@ -155,111 +159,168 @@ const ExamPage = () => {
     const examInfo = {
       title: "Welcome to the screening screen!",
       description: (
-          <div className="exam-info">
+        <div className="exam-info">
           <span className="exam-info-details">
             <HiOutlineClipboardDocumentList className="exam-icon" />
             20 problems
           </span>
-          </div>
+        </div>
       ),
     };
 
     return (
-        <div className="exam-page">
-          <ExamCard exam={examInfo} onStartExam={handleStart} loading={loading} />
-        </div>
+      <div className="exam-page">
+        <ExamCard exam={examInfo} onStartExam={handleStart} loading={loading} />
+      </div>
     );
   }
 
   const currentQuestion = questions[currentQuestionIndex];
 
+  const data = [
+    { name: "Yes", value: 66 },
+    { name: "No", value: 34 },
+  ];
+
+  const COLORS = ["rgb(24, 159, 204)", "rgb(199, 30, 30)"];
+
   return (
+    <Spin spinning={loading} tip="Processing your exam, please wait...">
+
       <div className="exam-page">
         <div className="header">
           <Timer
-              durationMinutes={duration}
-              onTimeout={() => {
-                Modal.warning({
-                  title: "Time's up!",
-                  content: "The test time has ended. Please submit your test.",
-                  onOk: handleFinishExam,
-                });
-              }}
-              sectionTitle={getCurrentSection()?.name}
-              totalQuestions={totalQuestions}
-              completedQuestions={completedQuestions}
+            durationMinutes={duration}
+            onTimeout={() => {
+              Modal.warning({
+                title: "Time's up!",
+                content: "The test time has ended. Please submit your test.",
+                onOk: handleFinishExam,
+              });
+            }}
+            sectionTitle={getCurrentSection()?.name}
+            totalQuestions={totalQuestions}
+            completedQuestions={completedQuestions}
           />
         </div>
         <div className="question-navigation">
           <Button
-              className="prev-next-btn"
-              onClick={handlePrevious}
-              disabled={currentQuestionIndex === 0}
+            className="prev-next-btn"
+            onClick={handlePrevious}
+            disabled={currentQuestionIndex === 0}
           >
             <IoIosArrowBack />
             PREV
           </Button>
           {getQuestionsForCurrentSection().map((_, index) => {
             const globalQuestionIndex =
-                sections
-                    .slice(0, getCurrentSection().sectionIndex)
-                    .reduce((sum, s) => sum + s.count, 0) + index;
+              sections
+                .slice(0, getCurrentSection().sectionIndex)
+                .reduce((sum, s) => sum + s.count, 0) + index;
 
             return (
-                <Button
-                    key={globalQuestionIndex}
-                    type={currentQuestionIndex === globalQuestionIndex ? "primary" : "default"}
-                    onClick={() => setCurrentQuestionIndex(globalQuestionIndex)}
-                    className={selectedAnswers[globalQuestionIndex] ? "selected-dot" : ""}
-                >
-                  {globalQuestionIndex + 1}
-                </Button>
+              <Button
+                key={globalQuestionIndex}
+                type={currentQuestionIndex === globalQuestionIndex ? "primary" : "default"}
+                onClick={() => setCurrentQuestionIndex(globalQuestionIndex)}
+                className={selectedAnswers[globalQuestionIndex] ? "selected-dot" : ""}
+              >
+                {globalQuestionIndex + 1}
+              </Button>
             );
           })}
 
           <Button
-              className="prev-next-btn"
-              onClick={handleNext}
-              disabled={currentQuestionIndex === questions.length - 1}
+            className="prev-next-btn"
+            onClick={handleNext}
+            disabled={currentQuestionIndex === questions.length - 1}
           >
             NEXT
             <IoIosArrowForward />
           </Button>
         </div>
         <QuestionCard
-            question={currentQuestion}
-            questionNumber={currentQuestionIndex + 1}
-            totalQuestions={questions.length}
-            selectedAnswer={selectedAnswers[currentQuestionIndex]}
-            onAnswerSelect={handleAnswerSelect}
-            examTitle={examTitle}
+          question={currentQuestion}
+          questionNumber={currentQuestionIndex + 1}
+          totalQuestions={questions.length}
+          selectedAnswer={selectedAnswers[currentQuestionIndex]}
+          onAnswerSelect={handleAnswerSelect}
+          examTitle={examTitle}
         />
         <div className="footer-completed">
           <Button
-              type="primary"
-              onClick={handleFinishExam}
-              disabled={completedQuestions !== totalQuestions}
+            type="primary"
+            onClick={handleFinishExam}
+            disabled={completedQuestions !== totalQuestions}
           >
             <BsFillStopFill />
             FINISH AND SUBMIT
           </Button>
         </div>
-
         <Modal
-            title="Test results"
-            visible={isModalVisible}
-            onOk={handleModalOk}
-            cancelButtonProps={{ style: { display: "none" } }}
+
+          visible={isModalVisible}
+          onOk={handleModalOk}
+          cancelButtonProps={{ style: { display: "none" } }}
+          width="50vw"
+          bodyStyle={{
+            height: "60vh",
+            padding: 0,
+          }}
         >
           {resultData && (
-              <div>
-                <p>ID: {resultData.id}</p>
+            <div className="rate-container">
+              <div className="title-pass-exam">
                 <p>{resultData.isPast ? "You have been accepted." : "You have failed."}</p>
+
+              </div>
+              <div className="your-point">
                 <p>Your Point: {resultData.point}</p>
               </div>
+
+              <div style={{ width: "100%", height: "100%" }} >
+                <PieChart width={1000} height={400}>
+                  <Pie
+                    data={data}
+                    dataKey="value"
+                    cx="45%"
+                    cy="75%"
+                    startAngle={180}
+                    endAngle={0}
+                    outerRadius={290}
+                    innerRadius={130}
+                    fill="#8884d8"
+                    label={({ name, percent }) =>
+                      `${name} ${Math.round(percent * 100)}%`
+                    }
+                  >
+                    {data.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={COLORS[index % COLORS.length]}
+                      />
+                    ))}
+                  </Pie>
+
+                  <Pie
+                    data={[{ value: 100 }]}
+                    dataKey="value"
+                    cx="50%"
+                    cy="100%"
+                    outerRadius={70}
+                    fill="#FFFFFF"
+                  />
+                </PieChart>
+              </div>
+              <div className="rate-container-time">
+                <p>Times: 20ph30s</p>
+              </div>
+            </div>
           )}
         </Modal>
       </div>
+    </Spin>
+
   );
 };
 
--- src/user/exam/components/ExamCard.jsx
+++ src/user/exam/components/ExamCard.jsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from "antd";
+import { Button, Spin, Typography } from "antd";
 import React from "react";
 import { FaPlay } from "react-icons/fa";
 import { IoMdInformationCircleOutline } from "react-icons/io";
@@ -8,43 +8,48 @@ const { Title, Paragraph } = Typography;
 
 const ExamCard = ({ exam, onStartExam, loading }) => {
     return (
-        <div style={{ position: "relative" }}>
-           
-            <div className="exam-card-container">
-                <Logo />
-                <div className="exam-card-header">
-                    <Title level={2} className="exam-title"><p>{exam.title}</p></Title>
-                    <Paragraph className="exam-description"><p>{exam.description}</p></Paragraph>
-                </div>
-                <div className="exam-card-body">
-                    <Paragraph className="instructions-title"><p>INSTRUCTIONS</p></Paragraph>
-                    <Paragraph className="instructions-text">
-                        1. This is an online Theory/Programming Exam.<br />
-                        2. Please make sure that you are using the latest version of the browser. We recommend using Google Chrome<br />
-                        3. It's mandatory to disable all the browser extensions and enable Add-ons and open the test in incognito mode.<br />
-                        4. For Practical Exam, you are free to choose your preferred programming language from the options that have been listed for you.
-                        Note that - All inputs are from STDIN and output to STDOUT.<br />
-                        5. To understand our test environment better, or know about other parameter like the time limits, etc you can read our FAQs.<br />
-                        6. To know the test results or figure out the next course of action, please contact your test administrator and they will guide you.<br/>
-                        Best wishes from Nafta! <br/>
-                    </Paragraph>
-                    <Button
-                        type="primary"
-                        onClick={onStartExam}
-                        className="start-exam-btn"
-                        loading={loading}
-                    >
-                        <p><FaPlay /> START TAKING THIS ASSIGMENT</p>
-                    </Button>
-                    <div className="instructions-info">
-                        <p><IoMdInformationCircleOutline /></p>
-                        <p><i>Please proceed to start taking this assignment.</i></p>
+        <Spin spinning={loading} tip="Loading exam please wait..." style={{
+            fontSize: "24px", // Tăng kích thước chữ
+            color: "#ffffff",
+            zIndex: 1000,
+        }}>
+
+            <div style={{ position: "relative" }} className={loading ? "blur-content" : ""}>
+
+                <div className="exam-card-container">
+                    <Logo />
+                    <div className="exam-card-header">
+                        <Title level={2} className="exam-title"><p>{exam.title}</p></Title>
+                        <Paragraph className="exam-description"><p>{exam.description}</p></Paragraph>
+                    </div>
+                    <div className="exam-card-body">
+                        <Paragraph className="instructions-title"><p>INSTRUCTIONS</p></Paragraph>
+                        <Paragraph className="instructions-text">
+                            1. This is an online Theory/Programming Exam.<br />
+                            2. Please make sure that you are using the latest version of the browser. We recommend using Google Chrome<br />
+                            3. It's mandatory to disable all the browser extensions and enable Add-ons and open the test in incognito mode.<br />
+                            4. For Practical Exam, you are free to choose your preferred programming language from the options that have been listed for you.
+                            Note that - All inputs are from STDIN and output to STDOUT.<br />
+                            5. To understand our test environment better, or know about other parameter like the time limits, etc you can read our FAQs.<br />
+                            6. To know the test results or figure out the next course of action, please contact your test administrator and they will guide you.<br />
+                            Best wishes from Nafta! <br />
+                        </Paragraph>
+                        <Button
+                            type="primary"
+                            onClick={onStartExam}
+                            className="start-exam-btn"
+                        >
+                            <p><FaPlay /> START TAKING THIS ASSIGMENT</p>
+                        </Button>
+                        <div className="instructions-info">
+                            <p><IoMdInformationCircleOutline /></p>
+                            <p><i>Please proceed to start taking this assignment.</i></p>
+                        </div>
                     </div>
                 </div>
+
             </div>
-
-        </div>
-
+        </Spin>
 
     );
 };
