c20da9c02a0a350cc22ecae523cf813810bd6a98
The-Quan <145833348+The-Quan@users.noreply.github.com>
2025-01-12 18:32:26 +0700

    ok send email

--- src/admin/listcandidate/ListManagerCandidate.jsx
+++ src/admin/listcandidate/ListManagerCandidate.jsx
@@ -1,10 +1,13 @@
 // ListManagerCandidate.jsx
 import { EyeOutlined } from '@ant-design/icons';
-import { Button, Modal, Spin, Table } from 'antd';
+import { Button, Modal, Spin, Table, DatePicker, TimePicker } from 'antd';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import React, { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 import './ListManagerCandidate.scss';
+import GlobalAlert from '../../common/globalAlert/GlobalAlert';
 
 export default function ListManagerCandidate() {
   const [candidates, setCandidates] = useState([]);
@@ -12,6 +15,15 @@ export default function ListManagerCandidate() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const token = Cookies.get("token");
   const [loading, setLoading] = useState(false);
+  const [showInterviewForm, setShowInterviewForm] = useState(false);
+  const [date, setDate] = useState(null);
+  const [time, setTime] = useState(null);
+  const [loadingg, setLoadingg] = useState(false);
+  const [alertType, setAlertType] = useState(null);
+  const [alertDescription, setAlertDescription] = useState(null);
+  const [visible, setVisible] = useState(false);
+  dayjs.extend(customParseFormat);
+
 
   useEffect(() => {
     setLoading(true);
@@ -42,6 +54,7 @@ export default function ListManagerCandidate() {
       })
       .then((response) => {
         setSelectedCandidate(response.data.data);
+        console.log(response.data.data)
         setIsModalVisible(true);
       })
       .catch((error) => {
@@ -104,10 +117,57 @@ export default function ListManagerCandidate() {
     },
 
   ];
+  const renderExamResult = (isPast) => {
+    if (isPast === false) {
+      return <span style={{ color: 'red' }}>Failed</span>;
+    } else if (isPast === true) {
+      return <span style={{ color: 'green' }}>Pass</span>;
+    } else {
+      return <span style={{ color: 'orange' }}>In progress</span>;
+    }
+  };
+
+  const onChanges = (date, dateString) => {
+    setDate(dateString);
+  };
+
+  const onChange = (time, timeString) => {
+    setTime(timeString);
+  };
+
+  const handleInterviewClick = () => {
+    setShowInterviewForm(!showInterviewForm);
+  };
+
+  const handleSendEmail = async (id, date, time) => {   
+    try {
+      const response = await axios.post(`http://46.202.178.139:5050/api/v1/candidates/send/email/interview/${id}`,{date, time});
+      
+      if(response) {
+        setAlertDescription("Emails sent successfully.");
+        setAlertType("success");
+        setVisible(true);
+        setLoadingg(false);  
+        setDate(null)
+        setTime(null)
+      } else {
+        setAlertDescription("Error sending email.");
+        setAlertType("error");
+        setVisible(true);
+      }
+    } catch (error) {
+      setAlertDescription("Error sending email.");
+      setAlertType("error");
+      setVisible(true);
+    } finally {
+      setLoadingg(false);  
+    }
+};
+
 
   return (
-
     <div className="list-manager-candidate">
+      <GlobalAlert setVisible={setVisible} visible={visible} type={alertType} description={alertDescription} />
       <h1>Candidate List</h1>
       <Spin spinning={loading} tip="Loading...">
         <Table
@@ -125,7 +185,7 @@ export default function ListManagerCandidate() {
         onCancel={handleCancel}
         footer={null}
         width={800}
-      >   
+      >
         {loading ? (
           <Spin spinning={loading} tip="Loading details..." />
         ) : (
@@ -139,12 +199,40 @@ export default function ListManagerCandidate() {
                   />
                 </div>
                 <div className="candidate-info">
-                  <h2>{selectedCandidate.fullName}</h2>
-                  <p><strong>Status:</strong> {selectedCandidate.status || 'Active'}</p>
+                  <div className='info'>
+                    <h2>{selectedCandidate.fullName}</h2>
+                    <p><strong>Status:</strong> {renderExamResult(selectedCandidate.isPast)}</p>
+                    <Button type="primary" onClick={handleInterviewClick}>Interview</Button>
+                  </div>
                   <div className="action-buttons">
-                    <Button type="primary">Interview</Button>
-                    <Button type="default">Reject</Button>
-                    <Button type="default">Mail</Button>
+                    {showInterviewForm && (
+                      <div className="interview-form">
+                        <div>
+                          <strong style={{ fontSize: "20px" }}>Interview schedule</strong>
+                          <p>Please select a schedule that is still available.</p>
+                        </div>
+                        <div className='btn-select'>
+                          <DatePicker
+                            format="YYYY-MM-DD"
+                            onChange={onChanges}
+                          />
+                          <TimePicker onChange={onChange} defaultOpenValue={dayjs('00:00:00', 'HH:mm:ss')} />
+                          <Button
+                            type="primary"
+                            loading={loadingg}
+                            onClick={() => {handleSendEmail(selectedCandidate.id, date, time); setLoadingg(true);}}
+                          >
+                            Send Email
+                          </Button>
+                        </div>
+
+                        <div className='btn-send'>
+
+                        </div>
+                      </div>
+                    )}
+                    {/* <Button type="default">Reject</Button>
+                    <Button type="default">Mail</Button> */}
                   </div>
                 </div>
               </div>
